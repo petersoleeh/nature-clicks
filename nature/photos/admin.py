2d52c2837ecb938e4970be4aac8b05753136a567
peter soleeh <petersoleeh@gmail.com>
2017-11-17 12:56:12 +0300

    create super user

--- nature/photos/admin.py
+++ nature/photos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post,tags,User
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(User)
+admin.site.register(Post,PostAdmin)
+admin.site.register(tags)
