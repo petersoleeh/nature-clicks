3f80c235e2ca20805ddce9b519ed78cd7e6e13ee
peter soleeh <petersoleeh@gmail.com>
2017-11-19 17:02:40 +0300

    url for single image details

--- nature/photos/urls.py
+++ nature/photos/urls.py
@@ -8,7 +8,11 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
 
     #The page to view a full size image and its details
-    url(r'^(?P<post_id>[0-9]+)/$',views.single_image,name='image_details')
+    url(r'^(?P<post_id>[0-9]+)/$',views.single_image,name='image_details'),
+
+    #contacts page
+    url(r'^contacts/$',views.contacts,name='contacts')
+
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
