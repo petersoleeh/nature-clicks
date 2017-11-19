f367691e541cf1804bc98289d352c871e33f42ed
peter soleeh <petersoleeh@gmail.com>
2017-11-19 17:02:12 +0300

    single image view

--- nature/photos/views.py
+++ nature/photos/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    posts = Post.all_posts()
+    return render(request,'index.html',{"posts":posts})
 
 def single_image(request,post_id):
-    return render(request,'single_image.html')
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404("The Post does not exist")
+
+    return render(request,'single_image.html',{"post":post})
+
+
+def contacts(request):
+    return render(request,'contacts.html')
