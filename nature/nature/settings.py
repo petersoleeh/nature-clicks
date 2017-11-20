ad7af4630f8d02ae650fa16ffc6ae7b7ad3a561f
peter soleeh <petersoleeh@gmail.com>
2017-11-20 14:35:10 +0300

    hope it works number 5

--- nature/nature/settings.py
+++ nature/nature/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -91,6 +91,10 @@ DATABASES = {
         'PASSWORD': 'soleeh..',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
